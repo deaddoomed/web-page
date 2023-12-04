023860f1135df8769d7083a2db481a88304c85cc
dead doomed <igna.gallardo@duocuc.cl>
2023-12-04 03:14:52 -0400

    1.1.0

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function consoleLog(){
-    console.log(window.navigator.appName);
-    console.log(window.navigator.platform);
-    console.log(window.navigator.product);
+function isMobile() {
+    const plataforms = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+    return plataforms.test(navigator.userAgent);
+  }
+ 
+function consoleLog(){  
+    if (isMobile()) {
+        console.log("Mobile device detected");
+    } else {
+        console.log("Desktop device detected");
+    }
 }
